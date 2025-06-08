92adba85e78b77f26c73fb406b42f5d47eb7364c
Ananda Irfansyah <anndairfansyah@gmail.com>
2025-06-08 17:26:58 +0700

    Update variables.dart

--- flutter_pos_app/lib/core/constants/variables.dart
+++ flutter_pos_app/lib/core/constants/variables.dart
@@ -1,4 +1,4 @@
 class Variables {
-  static const String baseUrl = 'http://192.168.100.149:8000';
+  static const String baseUrl = 'http://000.000.000.000:8000';
   static const String imageBaseUrl = '$baseUrl/storage/products/';
 }
